3ba7b7c8c1e757814b0277d07b2b36e99cd06f60
Songchen Li <songchen.li@thetradedesk.com>
2022-10-20 17:52:09 -0400

    Try push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in a Stormy Day
+          Learn React in a Snowy Day
         </a>
       </header>
     </div>
